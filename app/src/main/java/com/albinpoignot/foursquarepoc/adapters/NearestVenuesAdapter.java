03b353e809bf00aeab97755cecb6cd4cbae1bba4
Albin <mithrop@gmail.com>
2015-10-15 02:59:06 +0200

    Enhance UI + add missing getter/setter + add convertView and viewHolder usage

--- app/src/main/java/com/albinpoignot/foursquarepoc/adapters/NearestVenuesAdapter.java
+++ app/src/main/java/com/albinpoignot/foursquarepoc/adapters/NearestVenuesAdapter.java
@@ -8,6 +8,7 @@ import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 import com.albinpoignot.foursquarepoc.R;
+import com.albinpoignot.foursquarepoc.models.Category;
 import com.albinpoignot.foursquarepoc.models.CompactVenue;
 
 import java.util.ArrayList;
@@ -18,12 +19,12 @@ import java.util.List;
  */
 public class NearestVenuesAdapter extends BaseAdapter
 {
-    private Context context;
+    private LayoutInflater inflater;
     private List<CompactVenue> compactVenues;
 
     public NearestVenuesAdapter(Context context)
     {
-        this.context = context;
+        this.inflater = LayoutInflater.from(context);
         this.compactVenues = new ArrayList<>();
     }
 
@@ -52,17 +53,55 @@ public class NearestVenuesAdapter extends BaseAdapter
     @Override
     public View getView(int position, View convertView, ViewGroup parent)
     {
-        LayoutInflater inflater = (LayoutInflater) context
-                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        ViewHolder holder;
 
-        convertView= inflater.inflate(R.layout.list_item_compact_venue, null);
+        if (convertView == null)
+        {
+            convertView = inflater.inflate(R.layout.list_item_compact_venue, null);
 
-        TextView txtName=(TextView)convertView.findViewById(R.id.venue_name);
-        TextView txtLocation=(TextView)convertView.findViewById(R.id.venue_location);
+            holder = new ViewHolder();
+            holder.txtName = (TextView) convertView.findViewById(R.id.venue_name);
+            holder.txtDistance = (TextView) convertView.findViewById(R.id.venue_distance);
+            holder.txtCategories = (TextView) convertView.findViewById(R.id.venue_categories);
 
-        txtName.setText(compactVenues.get(position).getName());
-        txtLocation.setText(compactVenues.get(position).getLocation().getAddress());
+            convertView.setTag(holder);
+        }
+        else
+        {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        CompactVenue venue = compactVenues.get(position);
+
+        holder.txtName.setText(venue.getName());
+
+        if(venue.getLocation() != null && venue.getLocation().getDistance() != null)
+        {
+            holder.txtDistance.setText(venue.getLocation().getDistance());
+            holder.txtDistance.setVisibility(View.VISIBLE);
+        }
+        else
+        {
+            holder.txtDistance.setVisibility(View.GONE);
+        }
+
+        if(venue.getCategories() != null && venue.getCategories().size() > 0)
+        {
+            holder.txtCategories.setText(venue.getCategories().get(0).getName());
+            holder.txtDistance.setVisibility(View.VISIBLE);
+        }
+        else
+        {
+            holder.txtDistance.setVisibility(View.GONE);
+        }
 
         return convertView;
+    }
+
+    private static class ViewHolder
+    {
+        public TextView txtName;
+        public TextView txtDistance;
+        public TextView txtCategories;
     }
 }
